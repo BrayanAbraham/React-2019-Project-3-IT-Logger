34a0f4c3527cf8a74d48c109d97bb7b111ba4fca
BrayanAbraham <brayanabraham@gmail.com>
2020-03-27 20:29:19 +0530

    Redux Store & PRovider Setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React, { useEffect, Fragment } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 import SearchBar from "./components/layout/SearchBar";
@@ -19,17 +22,19 @@ const App = () => {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
